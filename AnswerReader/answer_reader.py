d27dd0cd2599614e7593726a47a8b599a49540c4
Harrison Knight <79892001+H20rj@users.noreply.github.com>
2024-12-12 11:34:22 -0600

    added answer_reader.py

---
+++ AnswerReader/answer_reader.py
@@ -0,0 +1,35 @@
+"""Takes Answers and cycles through them one by one"""
+
+
+def main() -> None:
+    """Main"""
+    answer_dict: dict[int, str] = {}
+    run: bool = True
+    string = input("Enter a list of answers:\n")
+    iterations: int = 0
+    for letter in string:
+        iterations += 1
+        answer_dict[iterations] = letter
+    iterations = 1
+
+    while run:
+        jump_to: int = input(
+            "Enter a number to jump to or press enter for next answer: "
+        )
+        if jump_to.isdigit():
+            print(f"{jump_to}: {answer_dict[int(jump_to)]}")
+            iterations = int(jump_to) + 1
+        elif jump_to == "q":
+            run = False
+        elif not jump_to.isdigit() and not jump_to == "":
+            print("Invalid input")
+        else:
+            if iterations > max(len(string)):
+                print("No more answers")
+            else:
+                print(f"{iterations}: {answer_dict[iterations]}")
+                iterations += 1
+
+
+if __name__ == "__main__":
+    main()
