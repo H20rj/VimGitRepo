5bbb02a167768758eb0dcf5cb7e81a82464d1952
Harrison Knight <79892001+H20rj@users.noreply.github.com>
2024-12-12 10:51:20 -0600

    guessinggame.py

--- GuessingGame/guessing_game.py
+++ GuessingGame/guessing_game.py
@@ -6,17 +6,20 @@ from random import randint
 def main() -> None:
     """Main"""
     number = randint(1, 25)
-
-    user_input = int(input("Enter a number 1-25: "))
+    counter = 0
+    try:
+        user_input = int(input("Enter a number 1-25: "))
+    except ValueError:
+        print("Please enter an integer")
     while user_input != number:
+        counter += 1
         if user_input > number:
             print("too high")
             user_input = int(input("Enter a number 1-25: "))
         elif user_input < number:
             print("too low")
             user_input = int(input("Enter a number 1-25: "))
-        elif user_input == number:
-            print("You got it!")
+    print(f"Congrats! You got it in {counter} tries!")
 
 
 if __name__ == "__main__":
